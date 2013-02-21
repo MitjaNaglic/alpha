c8bbad93c93c3df9fb5edc38824931b5fe269107
mitna <mitna@localhost>
2013-02-21 16:06:49 +0100

    initial

---
+++ Android/src/com/mitjanaglic/alpha/MainActivity.java
@@ -0,0 +1,18 @@
+package com.mitjanaglic.alpha;
+
+import android.os.Bundle;
+import com.badlogic.gdx.backends.android.AndroidApplication;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: mito
+ * Date: 21.2.2013
+ * Time: 15:50
+ * To change this template use File | Settings | File Templates.
+ */
+public class MainActivity extends AndroidApplication {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        initialize(new Game(), false);
+    }
+}
---
+++ Desktop/src/com/mitjanaglic/alpha/DesktopStarter.java
@@ -0,0 +1,16 @@
+package com.mitjanaglic.alpha;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: mito
+ * Date: 21.2.2013
+ * Time: 15:39
+ * To change this template use File | Settings | File Templates.
+ */
+public class DesktopStarter {
+    public static void main (String[] args) {
+        new LwjglApplication(new Game(), "Game", 480, 320, false);
+    }
+}
