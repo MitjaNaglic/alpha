83c5f2d42dd0e58e29258993661a6131717d48af
mitna <mitna@localhost>
2013-03-08 16:35:30 +0100

    .

---
+++ Util/src/Main.java
@@ -0,0 +1,7 @@
+import com.badlogic.gdx.tools.imagepacker.TexturePacker2;
+
+public class Main {
+            public static void main(String[] args) {
+                TexturePacker2.process("data/png", "data/png/textures", "textures.pack");
+            }
+}
