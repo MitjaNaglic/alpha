7e67ac2e07c78d2b257700147ddb932aec43f532
mitna <mitna@localhost>
2013-04-20 14:29:47 +0200

    .

--- Desktop/src/com/mitjanaglic/alpha/DesktopStarter.java
+++ Desktop/src/com/mitjanaglic/alpha/DesktopStarter.java
@@ -1,5 +1,6 @@
 package com.mitjanaglic.alpha;
 
+import com.badlogic.gdx.Files;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.mitjanaglic.alpha.game.Alpha;
@@ -20,6 +21,8 @@ public class DesktopStarter {
         config.width = 1280;
         config.height = 720;
         config.fullscreen = false;
+        config.resizable = false;
+        config.addIcon("data/png/icon/ic_launcher.png", Files.FileType.Internal);
         new LwjglApplication(new Alpha(), config);
     }
 }
